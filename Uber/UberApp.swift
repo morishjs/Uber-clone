b8378ceb908a7649e68492396b15a76d62638892
Junsuk Park <park64kr63@gmail.com>
2022-09-24 16:36:47 +0900

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Park Junsuk on 2022/09/24.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
